9e8519613da9355d47cd15e386c8c045fb82f190
Somu Gowda <somugowda.work@gmail.com>
2024-10-11 08:38:52 +0530

    added backend render api

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 //set the base URL for axios
 const api = axios.create({
-    baseURL: "http://localhost:5000/api/auth"
+    baseURL: "https://password-reset-backend-1-o356.onrender.com/api/auth"
 })
 
 export default api;
